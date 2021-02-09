9f28db027fcdd2853582345da419983ee9e187e1
ipatchcables <30351225+ipatchcables@users.noreply.github.com>
2021-02-09 14:26:56 -0500

    Update derp.py

--- web/derp.py
+++ web/derp.py
@@ -2,4 +2,5 @@ import BaseHTTPServer, SimpleHTTPServer
 import ssl
 
 httpd = BaseHTTPServer.HTTPServer(('0.0.0.0', 443), SimpleHTTPServer.SimpleHTTPRequestHandler)
-httpd.socket = ssl.wrap_socket(httpd.socket, certfile='/opt/web/fullchain.pem', keyfile='/opt/web/privkey.pem', server_$httpd.serve_forever()
+httpd.socket = ssl.wrap_socket(httpd.socket, certfile='/opt/web/fullchain.pem', keyfile='/opt/web/privkey.pem', server_side=True, ssl_version=ssl.PROTOCOL_TLSv1_2)
+httpd.serve_forever()
